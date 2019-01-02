2e0f967c751b78b9897b08d04f3bdf6d42fe8232
Monica <monicast@outlook.com>
2019-01-01 22:41:57 -0800

    It works now in a fairly flexible way

--- image_fun.py
+++ image_fun.py
@@ -1,52 +1,58 @@
 
 """
-This code loads an image and do stuff with it
+A not completely contrived demo.
+Demos
+- docstrings
+- opencv
+  - basic loading of images
+  - face finding
+  - drawing rectangles
+- numpy
+  - 2D arrays for images
+  - pass by reference
+- matplotlib
+  - showing images
+  - interactive and non-interactive mod
+- basic module structure
+  - calling from __main__
 """
 
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def find_faces(image_file='derry-girls.jpg'):
-    plt.ion()
-    img = cv2.imread(image_file, cv2.IMREAD_COLOR)
+def find_faces(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(img_gray, 1.3, 2)
+    face_rois = face_cascade.detectMultiScale(img_gray, 1.3, 3)
+    return face_rois
 
-    for (face_x, face_y, face_w, face_h) in faces:
-        cv2.rectangle(img, (face_x, face_y), (face_x+face_w, face_y+face_h), (255, 0, 0), 2)
-        roi_gray = img_gray[face_y:face_y+face_h, face_x:face_x+face_w]
-        roi_color = img[face_y:face_y+face_h, face_x:face_x+face_w]
+def add_rois_to_image(image, rois):
+    for (roi_x, roi_y, roi_w, roi_h) in rois:
+        cv2.rectangle(image, (roi_x, roi_y), (roi_x+roi_w, roi_y+roi_h), (255, 0, 0), 2)
 
-    fig = plt.figure("image fun")
-    plt.setp(axes, xticks=[], yticks=[])
-    plt.subplot(2, 2, 1)
-    # plt.imshow(np.stack((img[:, :, 2], img[:, :, 1], img[:, :, 0]), axis=2))
+def find_faces_and_display(image_file, interactive=True):
+    img = cv2.imread(image_file, cv2.IMREAD_COLOR)
+    if interactive:
+        plt.ion()
+    else:
+        plt.ioff()
+    fig = plt.figure("Face finding demo")
+    plt.subplot(1, 2, 1)
     plt.imshow(img[:,:,::-1])
-    plt.title('RGB Image')
+    plt.title('Image')
     plt.xticks([])
     plt.yticks([])
 
-    plt.subplot(2, 2, 3)
-    plt.imshow(img_gray, cmap='gray')
-    plt.title('Greyscale')
-    plt.xticks([])
-    plt.yticks([])
+    rois = find_faces(img)
+    add_rois_to_image(img, rois)
 
-    plt.subplot(2, 2, 2)
-    plt.imshow(edges, cmap='gray')
-    plt.title('Edges')
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.subplot(2, 2, 4)
-    plt.imshow(np.stack((img[:, :, 2], img[:, :, 1], img[:, :, 0]), axis=2))
-    plt.title('Derry Girl Faces')
+    plt.subplot(1, 2, 2)
+    plt.imshow(img[:,:,::-1])
+    plt.title('Image with ROIs')
     plt.xticks([])
     plt.yticks([])
     plt.show()
 
 if __name__ == "__main__":
-    pass
-     
+    find_faces_and_display('derry-girls.jpg', False) 
